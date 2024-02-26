7205f09e272b1c767f2374521912e2a4da4c71c4
João Pedro Bernanos <64542178+JBernanos@users.noreply.github.com>
2024-02-25 21:00:21 -0300

    Update bar.sh

--- i3/bar.sh
+++ i3/bar.sh
@@ -2,6 +2,6 @@
 
 while :
 do
-	echo $(upower -i /org/freedesktop/UPower/devices/battery_BAT0| grep -E "state|to\ faull|percentage")" "// $(date '+%Y-%m-%d %H:%M:%S')
+	echo "("$(upower -i /org/freedesktop/UPower/devices/battery_BAT0| grep -E "state|to\ faull|percentage")" ""|" $(date '+%Y-%m-%d %H:%M:%S')" )"
 	sleep 1
 done
